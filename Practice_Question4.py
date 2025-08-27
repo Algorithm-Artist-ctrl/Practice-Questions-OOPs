cb85dacc8238f501fee959d99c3b768aa326f987
Algorithm-Artist-ctrl <tarunkushwahkt@gmail.com>
2025-08-28 00:15:44 +0530

    Create a base class Vehicle and a child class Bike that inherits from Vehicle

---
+++ Practice_Question4.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    def details(self):
+        print("All are from TVS Honda")
+class Bike(Vehicle):
+    def details(self):
+        print("All are from TVS Honda")
+c1= Bike()
+c1.details()
