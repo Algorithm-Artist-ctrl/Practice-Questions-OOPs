97fe46debf9a4f4d0a8c47b14cc967a6234663c5
Algorithm-Artist-ctrl <tarunkushwahkt@gmail.com>
2025-08-28 21:55:12 +0530

    Create two classes Instagram and Twitter, both having a method post(), but with different messages

---
+++ Practice_Question11.py
@@ -0,0 +1,10 @@
+class Instagram:
+    def __init__(self):
+        print("Welcome to Instagram Profile")
+
+class Twitter:
+    def __init__(self):
+        print("Welcome to Twitter Profile")
+
+insta = Instagram()
+tw = Twitter()
